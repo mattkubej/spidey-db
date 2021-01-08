c857d8d78e3dd5cd50f6adfe48cb1acac56a70e7
Matt Kubej <mkubej@gmail.com>
2021-01-07 22:37:30 -0500

    refactor: variable name

--- server.c
+++ server.c
@@ -9,11 +9,11 @@
 #define TCP_BACKLOG 511
 
 int main() {
-  int sockfd, in_fd;
+  int sock_fd, in_fd;
   struct sockaddr_in servaddr;
   struct sockaddr in_addr;
 
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+  if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("failed to create socket");
     exit(EXIT_FAILURE);
   }
@@ -24,19 +24,19 @@ int main() {
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
   servaddr.sin_port = htons(PORT);
 
-  if (bind(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr)) < 0) {
+  if (bind(sock_fd, (struct sockaddr*) &servaddr, sizeof(servaddr)) < 0) {
       perror("failed to bind socket");
       exit(EXIT_FAILURE);
   }
 
-  if (listen(sockfd, TCP_BACKLOG) < 0) {
+  if (listen(sock_fd, TCP_BACKLOG) < 0) {
     perror("failed to listen");
     exit(EXIT_FAILURE);
   }
 
   socklen_t in_len = sizeof(in_addr);
 
-  if ((in_fd = accept(sockfd, &in_addr, &in_len)) < 0) {
+  if ((in_fd = accept(sock_fd, &in_addr, &in_len)) < 0) {
     perror("failed to accept");
     exit(EXIT_FAILURE);
   }
