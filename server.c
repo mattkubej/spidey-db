8278585fee26dff645054af53f2d4827d68b04be
Matt Kubej <mkubej@gmail.com>
2021-01-08 22:55:44 -0500

    feat: echo back input

--- server.c
+++ server.c
@@ -10,7 +10,7 @@
 #define TCP_BACKLOG 11
 
 int main() {
-  int serv_fd, in_fd, val_read;
+  int serv_fd, in_fd, read_size;
   struct sockaddr_in serv_addr, in_addr;
   int in_len = sizeof(in_addr);
 
@@ -21,6 +21,12 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
+  int enable = 1;
+  if (setsockopt(serv_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0) {
+    perror("setsockopt(SO_REUSEADDR) failed");
+    exit(EXIT_FAILURE);
+  }
+
   memset(&serv_addr, 0, sizeof(serv_addr));
 
   serv_addr.sin_family = AF_INET;
@@ -28,8 +34,8 @@ int main() {
   serv_addr.sin_port = htons(PORT);
 
   if (bind(serv_fd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
-      perror("failed to bind socket");
-      exit(EXIT_FAILURE);
+    perror("failed to bind socket");
+    exit(EXIT_FAILURE);
   }
 
   if (listen(serv_fd, TCP_BACKLOG) < 0) {
@@ -37,19 +43,24 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
-  while (1) {
-    printf("--- listening for connections ---\n\n");
+  int buff_size = 1024;
+  char client_buffer[buff_size];
 
+  while (1) {
+    printf("--- waiting for clients ---\n");
     if ((in_fd = accept(serv_fd, (struct sockaddr*) &in_addr, (socklen_t*) &in_len)) < 0) {
       perror("failed to accept");
       exit(EXIT_FAILURE);
     }
 
-    char buffer[30000] = {0};
-    val_read = read(in_fd, buffer, 30000);
-    printf("%s\n", buffer);
+    memset(client_buffer, 0, sizeof(client_buffer));
+    read_size = recv(in_fd, client_buffer, buff_size, 0);
+    if (read_size < 0) {
+      perror("read error");
+      exit(EXIT_FAILURE);
+    }
 
-    write(in_fd, pong, strlen(pong));
+    printf("%s\n", client_buffer);
     close(in_fd);
   }
 
