6664e653be6b1247feafa0290e0c4920af4e9e7e
Matt Kubej <mkubej@gmail.com>
2021-01-28 21:46:27 -0500

    refactor: remove print statement

--- src/commands.c
+++ src/commands.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <sys/socket.h>
 
-#include <stdio.h>
-
 void commandCommand(Request req, int clt_fd) {
   char *msg = "*1\r\n*6\r\n$7\r\ncommand\r\n:-1\r\n*2\r\n+loading\r\n+"
               "stale\r\n:0\r\n:0\r\n:0\r\n";
@@ -17,8 +15,6 @@ void commandPing(Request req, int clt_fd) {
   char *msg = "+PONG\r\n";
 
   int len = strlen(msg);
-
-  printf("sending :: %s\n", msg);
 
   send(clt_fd, msg, len, 0);
 }
