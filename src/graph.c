b7d1e462b73c791e0ffa0f08474ec5f7e5f2eae6
Matt Kubej <mkubej@gmail.com>
2021-02-06 18:19:44 -0500

    refactor: more appropriate name

--- src/graph.c
+++ src/graph.c
@@ -12,7 +12,7 @@ struct List {
 
 struct Graph {
   int size;
-  struct List *array;
+  struct List *list;
 };
 
 struct Node *createNode(int vertex) {
@@ -28,10 +28,10 @@ struct Graph *createGraph(int size) {
   struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
 
   graph->size= size;
-  graph->array = (struct List*) malloc(size* sizeof(struct List));
+  graph->list = (struct List*) malloc(size* sizeof(struct List));
 
   for (int i = 0; i < size; i++) {
-    graph->array[i].head = NULL;
+    graph->list[i].head = NULL;
   }
 
   return graph;
@@ -39,10 +39,10 @@ struct Graph *createGraph(int size) {
 
 void addEdge(struct Graph *graph, int src, int dest) {
   struct Node *node = createNode(dest);
-  node->next = graph->array[src].head;
-  graph->array[src].head = node;
+  node->next = graph->list[src].head;
+  graph->list[src].head = node;
 
   node = createNode(src);
-  node->next = graph->array[dest].head;
-  graph->array[dest].head = node;
+  node->next = graph->list[dest].head;
+  graph->list[dest].head = node;
 }
